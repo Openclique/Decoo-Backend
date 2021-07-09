e54d2f7204b57a5ddde1fea47c529b770136aec3
Openclique <arnaud.magnan@openclique.org>
2021-07-09 18:42:23 +0200

    First deploy after running some tests

---
+++ handler.js
@@ -0,0 +1,80 @@
+const AWS = require("aws-sdk");
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const places = express();
+
+// const PLACES_TABLE = process.env.PLACES_TABLE;
+// const dynamoDbClientParams = {};
+// if (process.env.IS_OFFLINE) {
+//   dynamoDbClientParams.region = 'localhost'
+//   dynamoDbClientParams.endpoint = 'http://localhost:8000'
+// }
+// const dynamoDbClient = new AWS.DynamoDB.DocumentClient(dynamoDbClientParams);
+
+app.use(express.json());
+places.use(express.json());
+
+places.get("/places/nearby/latitude/:latitude/longitude/:longitude", async function (req, res) {
+
+  res.json({ "latitude": req.params.latitude, "longitude": req.params.longitude });
+
+  // const params = {
+  //   TableName: PLACES_TABLE,
+  //   Key: {
+  //     userId: req.params.userId,
+  //   },
+  // };
+
+  // try {
+  //   const { Item } = await dynamoDbClient.get(params).promise();
+  //   if (Item) {
+  //     const { userId, name } = Item;
+  //     res.json({ userId, name });
+  //   } else {
+  //     res
+  //       .status(404)
+  //       .json({ error: 'Could not find user with provided "userId"' });
+  //   }
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).json({ error: "Could not retreive user" });
+  // }
+
+});
+
+// app.post("/users", async function (req, res) {
+//   const { userId, name } = req.body;
+//   if (typeof userId !== "string") {
+//     res.status(400).json({ error: '"userId" must be a string' });
+//   } else if (typeof name !== "string") {
+//     res.status(400).json({ error: '"name" must be a string' });
+//   }
+
+//   const params = {
+//     TableName: PLACES_TABLE,
+//     Item: {
+//       userId: userId,
+//       name: name,
+//     },
+//   };
+
+//   try {
+//     await dynamoDbClient.put(params).promise();
+//     res.json({ userId, name });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: "Could not create user" });
+//   }
+// });
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+
+module.exports.handler = serverless(app);
+module.exports.places = serverless(places);
