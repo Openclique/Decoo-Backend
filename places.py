2795d23d21ffb5bbd7e038f3068467b0c69bdaa8
Openclique <arnaud.magnan@openclique.org>
2021-07-13 17:31:14 +0200

    Please

--- places.py
+++ places.py
@@ -32,11 +32,3 @@ def nearby(event, context):
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
